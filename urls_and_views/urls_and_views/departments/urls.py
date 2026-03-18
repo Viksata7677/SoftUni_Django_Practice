0be1777ef19ebe68a2700b8fcfca03cb4f05c165
Viksata <viki1204394@gmail.com>
2026-03-18 17:20:38 +0200

    Created view with int:pk route

--- urls_and_views/urls_and_views/departments/urls.py
+++ urls_and_views/urls_and_views/departments/urls.py
@@ -4,5 +4,6 @@ from urls_and_views.departments import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:pk>/', views.view_with_int_pk),
     path('<variable>/', views.view_with_name),
 ]
--- urls_and_views/urls_and_views/departments/views.py
+++ urls_and_views/urls_and_views/departments/views.py
@@ -10,3 +10,7 @@ def index(request):
 
 def view_with_name(request, variable):
     return HttpResponse(f"<h1>Variable: {variable}</h1>")
+
+
+def view_with_int_pk(request, pk: int):
+    return HttpResponse(f"<h2>Pk: {pk}</h2>")
