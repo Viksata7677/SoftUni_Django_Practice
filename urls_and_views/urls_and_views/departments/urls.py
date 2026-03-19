8cb8581b962618dd7cdd85452edd6294dd09d420
Viksata <viki1204394@gmail.com>
2026-03-19 22:33:25 +0200

    Added include function to app urls

--- urls_and_views/urls_and_views/departments/urls.py
+++ urls_and_views/urls_and_views/departments/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
 from urls_and_views.departments import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:pk>/', views.view_with_int_pk),
-    path('<slug:slug>/', views.view_with_slug),
+    path('numbers/', include([
+        path('<int:pk>/', views.view_with_int_pk),
+        path('<slug:slug>/', views.view_with_slug),
+    ])),
     path('<variable>/', views.view_with_name),
-    path('<path:variable>/', views.view_with_name)
+    path('<path:variable>/', views.view_with_name),
 ]
