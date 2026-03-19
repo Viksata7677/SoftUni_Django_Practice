9c28daa5e76832de051615a22d724dd04736bc74
Viksata <viki1204394@gmail.com>
2026-03-19 22:22:35 +0200

    Created view with slug

--- urls_and_views/urls_and_views/departments/urls.py
+++ urls_and_views/urls_and_views/departments/urls.py
@@ -5,5 +5,6 @@ from urls_and_views.departments import views
 urlpatterns = [
     path('', views.index),
     path('<int:pk>/', views.view_with_int_pk),
+    path('<slug:slug>/', views.view_with_slug),
     path('<variable>/', views.view_with_name),
 ]
--- urls_and_views/urls_and_views/departments/views.py
+++ urls_and_views/urls_and_views/departments/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from urls_and_views.departments.models import Department
+
+
 # Create your views here.
 
 
@@ -14,3 +17,8 @@ def view_with_name(request, variable):
 
 def view_with_int_pk(request, pk: int):
     return HttpResponse(f"<h2>Pk: {pk}</h2>")
+
+
+def view_with_slug(request, slug):
+    department = Department.objects.get(slug=slug)
+    return HttpResponse(f'<h3>Department from slug: {department}</h3>')
