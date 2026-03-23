262b4cc8906db5bf8e39062f1e693b4d32f7fcd9
Viksata <viki1204394@gmail.com>
2026-03-23 21:27:28 +0200

    Added more fields to PersonForm

--- forum_app/forum_app/posts/forms.py
+++ forum_app/forum_app/posts/forms.py
@@ -2,5 +2,14 @@ from django import forms
 
 
 class PersonForm(forms.Form):
-    person_name = forms.CharField()
+    STATUS_CHOICE = (
+        (1, 'Draft'),
+        (2, 'Published'),
+        (3, 'Archived')
+    )
+
+    person_name = forms.CharField(label="Add person's name", widget=forms.TextInput(attrs={'placeholder': 'Search'}))
     age = forms.IntegerField()
+    date = forms.DateField()
+    is_lecturer = forms.BooleanField()
+    status = forms.IntegerField(widget=forms.Select(choices=STATUS_CHOICE))
