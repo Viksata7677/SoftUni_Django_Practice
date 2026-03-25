eac43db217bc051f9b242afc0cd38fc81be46d5e
Viksata <viki1204394@gmail.com>
2026-03-25 20:59:09 +0200

    Created model for comments

--- forum_app/forum_app/posts/models.py
+++ forum_app/forum_app/posts/models.py
@@ -12,3 +12,10 @@ class Post(models.Model):
     author = models.CharField(max_length=30)
     created_at = models.DateTimeField(auto_now_add=True)
     languages = models.CharField(max_length=20, choices=LanguageChoice.choices, default=LanguageChoice.OTHER)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
