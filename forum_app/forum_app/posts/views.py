3101be937aefcfb1a488794b059607bf3d8ea942
Viksata <viki1204394@gmail.com>
2026-03-30 22:40:07 +0300

    Create decorator for measuring time for request

--- forum_app/forum_app/posts/views.py
+++ forum_app/forum_app/posts/views.py
@@ -5,15 +5,18 @@ from django.forms.models import modelform_factory
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, RedirectView, ListView, FormView, CreateView, UpdateView, DeleteView, \
     DetailView
 
+from forum_app.decorators import measure_execution_time
 from forum_app.posts.forms import PersonForm, PostBaseForm, PostEditForm, PostDeleteForm, CommentFormSet
 from forum_app.posts.models import Post
 
 
 # Create your views here.
 
+@method_decorator(measure_execution_time, name='dispatch')
 class IndexView(TemplateView):
     template_name = 'common/index.html'  # static way
 
@@ -55,6 +58,7 @@ class DashboardView(ListView):
     model = Post
     template_name = 'posts/dashboard.html'
     context_object_name = 'posts'
+
 
 # FBV
 # def dashboard(request):
@@ -120,27 +124,6 @@ class EditPostView(UpdateView):
 #     return render(request, 'posts/edit-post.html', context)
 
 
-def details_page(request, pk:int):
-    post = Post.objects.get(pk=pk)
-    formset = CommentFormSet(request.POST)
-
-    if request.method == 'POST':
-        if formset.is_valid():
-            for form in formset:
-                if form.cleaned_data:
-                    comment = form.save(commit=False)
-                    comment.post = post
-                    comment.save()
-
-            return redirect('dashboard')
-    context = {
-        'post': post,
-        'formset': formset
-    }
-
-    return render(request, 'posts/details-page.html', context)
-
-
 class DeletePostView(DeleteView):
     model = Post
     template_name = 'posts/delete-post.html'
@@ -195,8 +178,30 @@ class PostDetailView(DetailView):
 
         return self.render_to_response(context)
 
+
+# def details_page(request, pk:int):
+#     post = Post.objects.get(pk=pk)
+#     formset = CommentFormSet(request.POST)
+#
+#     if request.method == 'POST':
+#         if formset.is_valid():
+#             for form in formset:
+#                 if form.cleaned_data:
+#                     comment = form.save(commit=False)
+#                     comment.post = post
+#                     comment.save()
+#
+#             return redirect('dashboard')
+#     context = {
+#         'post': post,
+#         'formset': formset
+#     }
+#
+#     return render(request, 'posts/details-page.html', context)
+
+
 class MyRedirectHomeView(RedirectView):
     url = reverse_lazy('homepage')  # static way
 
     # def get_redirect_url(self, *args, **kwargs):  # dynamic way
-    #     pass
+    # pass
